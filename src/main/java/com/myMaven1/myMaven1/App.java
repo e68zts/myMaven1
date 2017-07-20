b86206b0036eaf98ecbb3030141dcc1babc6238d
zhaotisheng <912980115@qq.com>
2017-07-20 16:28:12 +0800

    ss

--- src/main/java/com/myMaven1/myMaven1/App.java
+++ src/main/java/com/myMaven1/myMaven1/App.java
@@ -1,13 +1,19 @@
 package com.myMaven1.myMaven1;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    @SuppressWarnings("deprecation")
+	public static void main( String[] args )
     {
         System.out.println( "Hello World! 2222 666" );
+        Date date = new Date();
+        int hours = date.getHours();
+        System.out.println(hours);
     }
 }
